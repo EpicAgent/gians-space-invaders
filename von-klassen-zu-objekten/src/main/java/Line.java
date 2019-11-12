98c75db848197a10618c674f41c7803742520c03
Mathias Rieder <mathu.rieder@gmail.com>
2019-11-12 08:40:18 +0100

    Code der Aufträge zu "von Klasse zu Objekten" hinzugefügt.

--- von-klassen-zu-objekten/src/main/java/Line.java
+++ von-klassen-zu-objekten/src/main/java/Line.java
@@ -1,7 +1,9 @@
 public class Line {
+
     public static double calculateDistance(Point p1, Point p2){
         return Math.sqrt((p2.getY() - p1.getY()) * (p2.getY() - p1.getY()) +
                 (p2.getX() - p1.getX()) * (p2.getX() - p1.getX()));
     }
+
 }
 
